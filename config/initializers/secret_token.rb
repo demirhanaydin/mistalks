9631af56789e9b23cf494ca2f65aec1738853fc9
Demirhan Aydin <demirhanaydin@gmail.com>
2013-12-12 00:16:59 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mistalks::Application.config.secret_key_base = '55111def4f6040490932d72ccb76034e185586e8c2ed6836898bc8ea435944314bb95ccc394b7b84c7e2c5b3007dd0f283fcd69104d2e2cfb6c112988fe8ec12'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mistalks::Application.config.session_store :cookie_store, key: '_mistalks_session'
