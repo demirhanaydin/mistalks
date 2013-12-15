9be5d8d93602fd4b947f89383eae1678d5f3e7d9
Demirhan Aydin <demirhanaydin@gmail.com>
2013-12-15 02:08:45 +0200

    added about & contact page templates

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,12 @@ class WelcomeController < ApplicationController
   def index
     redirect_to :posts
   end
+
+  def about
+    
+  end
+
+  def contact
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Mistalks::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
-  # posts routes
-  resource :posts
+  # static pages
+  get 'about' => 'welcome#about'
+  get 'contact' => 'welcome#contact'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
